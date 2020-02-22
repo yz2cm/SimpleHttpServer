e3a89968b958ae84aee7c6cc6dde04034b17a30b
yz2cm <yz2cmjp@gmail.com>
2020-02-22 20:13:17 +0900

    Update log format.

--- SimpleHttpServer/Domain/HttpServer.cs
+++ SimpleHttpServer/Domain/HttpServer.cs
@@ -27,13 +27,11 @@ namespace SimpleHttpServer.Domain
 
             while (true)
             {
-                Console.WriteLine($"listening ... (port {this.port})");
+                Console.WriteLine($"[TCP] listening ... (port {this.port})");
                 using (var client = listener.AcceptTcpClient())
                 using (var stream = client.GetStream())
                 {
-                    Console.WriteLine();
-                    Console.WriteLine($"**** Accept from {client.Client.RemoteEndPoint} ***");
-                    Console.WriteLine();
+                    Console.WriteLine($"[TCP] Tcp connection established from {client.Client.RemoteEndPoint}.");
 
                     string httpRequest = string.Empty;
 
@@ -41,6 +39,8 @@ namespace SimpleHttpServer.Domain
                     {
                         byte[] bufferRead = new Byte[4096];
                         int readSize = stream.Read(bufferRead, 0, bufferRead.Length);
+
+                        Console.WriteLine($"[TCP] Read {readSize} bytes from Tcp stream.");
 
                         if (readSize == 0)
                         {
@@ -57,22 +57,32 @@ namespace SimpleHttpServer.Domain
 
                     if (httpRequest.Length == 0)
                     {
+                        Console.WriteLine($"[TCP] Read Empty bytes from Tcp stream.");
+
+                        client.Close();
                         continue;
                     }
 
-                    Console.WriteLine(">>>> Http Request :");
+                    Console.WriteLine("[HTTP] Http Request received.");
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
                     Console.WriteLine(httpRequest);
+                    Console.ResetColor();
                     Console.WriteLine();
 
                     var request = new HttpRequest(httpRequest);
                     var routeFullPath = new RouteFullPath(request.HttpRequestLine.RequestUri);
                     var httpResponse = this.routingTable.GetHtpResponse(routeFullPath);
 
-                    Console.WriteLine("<<<< Http Response :");
+                    Console.WriteLine("[HTTP] Http Response sent.");
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
                     Console.WriteLine(httpResponse);
+                    Console.ResetColor();
                     Console.WriteLine();
 
-                    stream.Write(httpResponse.ToBytes(), 0, httpResponse.ToBytes().Length);
+                    var resopnseBytes = httpResponse.ToBytes();
+                    stream.Write(resopnseBytes, 0, resopnseBytes.Length);
+
+                    Console.WriteLine($"[TCP] Write {resopnseBytes.Length} bytes to Tcp stream.");
                 }
             }
         }
